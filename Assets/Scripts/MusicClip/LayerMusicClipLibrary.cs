1c14716a219a8a2871a5e3caf406f36cdfd2baac
Ermis Chalkiadakis <ermis@talespin.company>
2020-11-01 20:18:55 +0100

    Created assets for all music clip libraries

--- Assets/Scripts/MusicClip/LayerMusicClipLibrary.cs
+++ Assets/Scripts/MusicClip/LayerMusicClipLibrary.cs
@@ -1,18 +1,6 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class LayerMusicClipLibrary : MonoBehaviour
+public class LayerMusicClipLibrary : ScriptableObject
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    [SerializeField] private LayerMusicClip[] layerMusicClips;
 }

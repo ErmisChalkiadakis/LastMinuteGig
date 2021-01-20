57b2845822e09a54b09a26e20255c50cd05c7205
Ermis Chalkiadakis <ermis@talespin.company>
2021-01-20 23:27:52 +0100

    Adjusted the volume of the input clips

--- Assets/Scripts/MusicMixer.cs
+++ Assets/Scripts/MusicMixer.cs
@@ -96,6 +96,7 @@ public class MusicMixer : MonoBehaviour
             child = new GameObject($"Input Player {i}");
             child.transform.parent = transform;
             inputAudioSources[i] = child.AddComponent<AudioSource>();
+            inputAudioSources[i].volume = 0.8f;
         }
 
         for (int i = 0; i < MAX_LAYERED_CLIPS; i++)
